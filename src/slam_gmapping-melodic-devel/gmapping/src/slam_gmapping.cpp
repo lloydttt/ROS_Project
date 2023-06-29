a1dda23d3fb29b08e44faa5fe6623a27e2068c01
lloydttt <126597991+lloydttt@users.noreply.github.com>
2023-06-29 22:54:57 +0800

    slam complete 1

--- src/slam_gmapping-melodic-devel/gmapping/src/slam_gmapping.cpp
+++ src/slam_gmapping-melodic-devel/gmapping/src/slam_gmapping.cpp
@@ -268,12 +268,12 @@ void SlamGMapping::init()
 
 void SlamGMapping::startLiveSlam()
 {
-  entropy_publisher_ = private_nh_.advertise<std_msgs::Float64>("entropy", 1, true);//gai
-  sst_ = node_.advertise<nav_msgs::OccupancyGrid>("map", 1, true);//gai
-  sstm_ = node_.advertise<nav_msgs::MapMetaData>("map_metadata", 1, true);//gai
+  entropy_publisher_ = private_nh_.advertise<std_msgs::Float64>("entropy", 1000, true);//gai
+  sst_ = node_.advertise<nav_msgs::OccupancyGrid>("map", 1000, true);//gai
+  sstm_ = node_.advertise<nav_msgs::MapMetaData>("map_metadata", 1000, true);//gai
   ss_ = node_.advertiseService("dynamic_map", &SlamGMapping::mapCallback, this);
-  scan_filter_sub_ = new message_filters::Subscriber<sensor_msgs::LaserScan>(node_, "scan", 5);//xiugai
-  scan_filter_ = new tf::MessageFilter<sensor_msgs::LaserScan>(*scan_filter_sub_, tf_, odom_frame_, 5);//xiugai
+  scan_filter_sub_ = new message_filters::Subscriber<sensor_msgs::LaserScan>(node_, "scan", 5000);//xiugai
+  scan_filter_ = new tf::MessageFilter<sensor_msgs::LaserScan>(*scan_filter_sub_, tf_, odom_frame_, 5000);//xiugai
   scan_filter_->registerCallback(boost::bind(&SlamGMapping::laserCallback, this, _1));
 
   transform_thread_ = new boost::thread(boost::bind(&SlamGMapping::publishLoop, this, transform_publish_period_));
@@ -283,9 +283,9 @@ void SlamGMapping::startReplay(const std::string & bag_fname, std::string scan_t
 {
   double transform_publish_period;
   ros::NodeHandle private_nh_("~");
-  entropy_publisher_ = private_nh_.advertise<std_msgs::Float64>("entropy", 1, true);//gai
-  sst_ = node_.advertise<nav_msgs::OccupancyGrid>("map", 1, true);//gai
-  sstm_ = node_.advertise<nav_msgs::MapMetaData>("map_metadata", 1, true);//gai
+  entropy_publisher_ = private_nh_.advertise<std_msgs::Float64>("entropy", 1000, true);//gai
+  sst_ = node_.advertise<nav_msgs::OccupancyGrid>("map", 1000, true);//gai
+  sstm_ = node_.advertise<nav_msgs::MapMetaData>("map_metadata", 1000, true);//gai
   ss_ = node_.advertiseService("dynamic_map", &SlamGMapping::mapCallback, this);
   
   rosbag::Bag bag;
@@ -319,7 +319,7 @@ void SlamGMapping::startReplay(const std::string & bag_fname, std::string scan_t
         s_queue.push(std::make_pair(s, ""));
       }
       // Just like in live processing, only process the latest 5 scans
-      if (s_queue.size() > 5) {//xiugaiiiiiiiiiiii
+      if (s_queue.size() > 5000) {//xiugaiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii
         ROS_WARN_STREAM("Dropping old scan: " << s_queue.front().second);
         s_queue.pop();
       }
